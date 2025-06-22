ff83467cb9a0a5f2e81596e88a0e0c14b7fdbfa3
viveksingh <viveksb1207@gmail.com>
2025-06-22 12:48:58 +0530

    add quick sort

---
+++ DSA_JAVA8_Features/src/main/java/org/DSA_Question/SortingSearching/QuickSort.java
@@ -0,0 +1,39 @@
+package org.DSA_Question.SortingSearching;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void sort(int[] nums, int low, int hi) {
+        if (low >= hi) return;
+
+
+        int s = low;
+        int e = hi;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
+
+        while (s <= e) {
+            while (nums[s] < pivot) s++;
+            while (nums[e] > pivot) e--;
+
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        sort(nums, low, e);
+        sort(nums, s, hi);
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {2,5,4,8,9,90,32,23,43,55,67,89,22};
+
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
