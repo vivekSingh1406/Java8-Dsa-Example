767687a1e4c782851f017c3c8171afb6603dcafa
viveksingh1406 <viveksb1207@gmail.com>
2025-06-30 23:28:42 +0530

    add some more question in a array

---
+++ DSA_JAVA8_Features/src/main/java/org/ArrayString/HarmoniousSubsequence.java
@@ -0,0 +1,35 @@
+package org.ArrayString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HarmoniousSubsequence {
+
+    public static int findLHS(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        int maxLength = 0;
+        for (int key : freqMap.keySet()) {
+            if (freqMap.containsKey(key + 1)) {
+                int length = freqMap.get(key) + freqMap.get(key + 1);
+                maxLength = Math.max(maxLength, length);
+            }
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3, 2, 2, 5, 2, 3, 7};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {1, 1, 1, 1};
+
+        System.out.println(findLHS(nums1)); // Output: 5
+        System.out.println(findLHS(nums2)); // Output: 2
+        System.out.println(findLHS(nums3)); // Output: 0
+    }
+}
+
---
+++ DSA_JAVA8_Features/src/main/java/org/ArrayString/LongestStrictlyIncreasing.java
@@ -0,0 +1,33 @@
+package org.ArrayString;
+
+public class LongestStrictlyIncreasing {
+
+    public static int longestMonotonicSubarray(int[] nums) {
+        int maxLen = 1;
+        int curr = 1;
+
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i-1] < nums[i]) curr++;
+            else curr = 1;
+
+            maxLen = Math.max(maxLen, curr);
+        }
+
+        curr = 1;
+        for(int i=1; i<nums.length; i++) {
+            if(nums[i-1] > nums[i]) curr++;
+            else curr = 1;
+
+            maxLen = Math.max(maxLen, curr);
+        }
+
+        return maxLen;
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {1,4,3,3,2};
+        int[] arr1 = {3,2,1};
+        System.out.println(longestMonotonicSubarray(arr));
+        System.out.println(longestMonotonicSubarray(arr1));
+    }
+}
